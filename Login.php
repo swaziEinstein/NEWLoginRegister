62d58d8ed1e227ab34a079ac395a34ecb8a077f6
swaziEinstein <31405680+swaziEinstein@users.noreply.github.com>
2017-08-28 12:41:24 +0200

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("mysql10.000webhost.com", "a3288368_user", "abcd1234", "a3288368_data");
+    $con = mysqli_connect("localhost", "id2730247_welcome	", "6576035a", "id2730247_sakhile");
     
     $username = $_POST["username"];
     $password = $_POST["password"];
